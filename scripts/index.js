8eedbc406e88d8f8303ca6e38604bebb0c76d645
Maxim Repkin <repkinm@mail.ru>
2022-11-14 23:31:22 +0300

    1

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__button-exit");
+const popupOpenButtonElement = document.querySelector(".profile__edit-button");
+const popupSaveButtonElement = document.querySelector(".popup__button-save");
+
+const openPopup = function () {
+    popupElement.classList.add("popup_is-opened");
+
+}
+const closePopup = function () {
+    popupElement.classList.remove("popup_is-opened");
+}
+const savePopup = function() {
+    popupElement.classList.remove("popup_is-opened");
+}
+popupOpenButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+popupSaveButtonElement.addEventListener("click", savePopup);
+
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__job");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', formSubmitHandler);
+
+
