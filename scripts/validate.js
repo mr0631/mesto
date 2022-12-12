af9d34ae8f05092f9256da1d2536693d20dd43de
Maxim Repkin <repkinm@mail.ru>
2022-12-12 13:53:30 +0300

    1

---
+++ scripts/validate.js
@@ -0,0 +1,50 @@
+const elementsValidators = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button-save',
+    inactiveButtonClass: 'popup__button-save_invalid',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visibility'
+  };
+  const checkInputValidity = (input, {errorClass, inputErrorClass}) => {
+    const error = document.querySelector(`#${input.id}-error`);
+    if(input.validity.valid) {
+        error.textContent = '';
+        error.classList.remove(errorClass);
+        input.classList.remove(inputErrorClass);
+    } else {
+        error.textContent = input.validationMessage;
+        error.classList.add(errorClass);
+        input.classList.add(inputErrorClass);
+
+    }
+}
+const toggleSaveButton = (inputs, button, {inactiveButtonClass}) => {
+    const isFormValid = inputs.every(input => input.validity.valid)
+
+    if(isFormValid) {
+        button.classList.remove(inactiveButtonClass);
+        button.disabled = '';
+
+    } else {
+        button.classList.add(inactiveButtonClass);
+        button.disabled = 'disabled';
+    }
+}
+const enableValidation = ({formSelector, inputSelector, submitButtonSelector, ...restConfig} ) =>  {
+const forms = [...document.querySelectorAll(formSelector)]  
+forms.forEach(form => {
+    const inputs = [...form.querySelectorAll(inputSelector)]
+    const button = form.querySelector(submitButtonSelector)
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    })
+    inputs.forEach(input => {
+        input.addEventListener('input', () => {
+            checkInputValidity(input, restConfig);
+            toggleSaveButton(inputs, button, restConfig);
+        })
+    })
+})
+};
+enableValidation(elementsValidators);
